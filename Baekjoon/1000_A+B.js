02e63f71b7de4a7e8aede70c2b5a10df264e36a2
Ryan <cho8213456@gmail.com>
2021-07-14 00:50:01 +0900

    Baekjoon 1000

---
+++ Baekjoon/1000_A+B.js
@@ -0,0 +1,20 @@
+// Input and Output
+
+const fs = require("fs");
+const stdin = (
+  process.platform === "linux"
+    ? fs.readFileSync("/dev/stdin").toString()
+    : ``
+).split("\n");
+
+const input = (() => {
+  let line = 0;
+  return () => stdin[line++];
+})();
+
+// #1000 Baekjoon JavaScript 
+
+const AB = input().split(' ')
+const res = Number(AB[0]) + Number(AB[1])
+
+console.log(res)
