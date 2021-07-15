9fb347c7df96ff7bf050cf691a9726b78f07b46c
blossom4 <cho8213456@gmail.com>
2021-07-16 02:05:50 +0900

    Baekjoon 14467

--- Baekjoon/14467_소가길을건너간이유1.js
+++ Baekjoon/14467_소가길을건너간이유1.js
@@ -5,8 +5,7 @@ const fs = require("fs");
 const stdin = (
   process.platform === "linux"
     ? fs.readFileSync("/dev/stdin").toString()
-    : `walking
-nia`
+    : ``
 ).split("\n");
 
 const input = (() => {
