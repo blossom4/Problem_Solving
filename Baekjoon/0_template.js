36587fba78aadb8d5202f2c13259a9183f3324e8
Ryan <cho8213456@gmail.com>
2021-07-15 14:54:59 +0900

    Baekjoon template

---
+++ Baekjoon/0_template.js
@@ -0,0 +1,20 @@
+////////////////////////////////////////////////////////////
+
+// Input and Output
+const fs = require("fs");
+const stdin = (
+  process.platform === "linux"
+    ? fs.readFileSync("/dev/stdin").toString()
+    : ``
+).split("\n");
+
+const input = (() => {
+  let line = 0;
+  return () => stdin[line++];
+})();
+
+////////////////////////////////////////////////////////////
+
+
+// Baekjoon JavaScript 
+// #
