4d7af6466ae44dbc558488ef3cb9189e3084839c
blossom4 <cho8213456@gmail.com>
2021-07-16 16:40:20 +0900

    Baekjoon 1622 Solved

--- Baekjoon/1622_공통순열.js
+++ Baekjoon/1622_공통순열.js
@@ -5,7 +5,12 @@ const fs = require("fs");
 const stdin = (
   process.platform === "linux"
     ? fs.readFileSync("/dev/stdin").toString()
-    : ``
+    : `pretty
+women
+walking
+down
+the
+street`
 ).split("\n");
 
 const input = (() => {
@@ -21,20 +26,37 @@ const input = (() => {
 
 // https://www.acmicpc.net/problem/1622
 
+while (1) {
+  // 입력 a와 b에 대하여 undefined나 null이 들어오면 즉시 종료시키고,
+  // 둘중 하나라도 빈문자열 ''이 들어오면 결과는 ''이므로 출력하고 다음 case로 넘어가도록 했다.
+  let a = input()
+  if (a == undefined || a == null) {
+    break
+  }
+  a = a.split('')
 
-const a = input().split('')
-const b = input().split('')
-const arr = []
+  let b = input()
+  if (b == undefined || b == null) {
+    break
+  }
+  b = b.split('')
 
-// b와 a를 이중for문을 돌며 중복되지 않도록 같은 알파벳들을 arr에 추가한다.
-for (let i = 0; i < b.length; i++) {
-  for (let j = 0; j < a.length; j++) {
-    if (b[i] == a[j]) {
-      arr.push(a[j])
-      a.splice(j, 1)
-      break
+  if (a == '' || b == '') {
+    console.log('')
+    continue
+  }
+
+  const arr = []
+    // b와 a를 이중for문을 돌며 중복되지 않도록 같은 알파벳들을 arr에 추가한다.
+  for (let i = 0; i < b.length; i++) {
+    for (let j = 0; j < a.length; j++) {
+      if (b[i] == a[j]) {
+        arr.push(a[j])
+        a.splice(j, 1)
+        break
+      }
     }
   }
+  
+  console.log(arr.sort().join(''))
 }
-
-console.log(arr.sort().join(''))
