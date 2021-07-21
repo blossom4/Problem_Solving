49cd68c289b208ce32c33947a054ad06221a1864
blossom4 <cho8213456@gmail.com>
2021-07-22 01:33:14 +0900

    Baekjoon 20665 Unsolved

---
+++ Baekjoon/20665_독서실거리두기.js
@@ -0,0 +1,24 @@
+////////////////////////////////////////////////////////////
+
+// Input and Output
+const fs = require("fs");
+const stdin = (
+  process.platform === "linux"
+    ? fs.readFileSync("/dev/stdin").toString()
+    : ``
+).split("\n");
+
+const input = (() => {
+  let line = 0;
+  return () => stdin[line++];
+})();
+
+////////////////////////////////////////////////////////////
+
+
+// Baekjoon JavaScript 
+// #20665  독서실 거리두기
+
+// https://www.acmicpc.net/problem/20665
+
+
