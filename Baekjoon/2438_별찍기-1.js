13c114713864e7fcb0c66d00d1bb36daebe1028f
blossom4 <cho8213456@gmail.com>
2021-07-25 01:34:40 +0900

    Baekjoon 2438 Solved

--- Baekjoon/2438_별찍기-1.js
+++ Baekjoon/2438_별찍기-1.js
@@ -17,7 +17,18 @@ const input = (() => {
 
 
 // Baekjoon JavaScript 
-// #2438
+// #2438  별 찍기 - 1
 
 // https://www.acmicpc.net/problem/2438
 
+
+const N = Number(input())
+let temp = ''
+let ans = ''
+for (let i = 0; i < N; i++) {
+  temp += '*'
+  ans += temp
+  if (i != N) ans += '\n'
+}
+
+console.log(ans)
