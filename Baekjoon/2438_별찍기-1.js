ad78fa835133442ab90c075a1dafc67f68fb50c1
blossom4 <cho8213456@gmail.com>
2021-07-24 23:27:16 +0900

    Baekjoon 2438 Unsolved

---
+++ Baekjoon/2438_별찍기-1.js
@@ -0,0 +1,23 @@
+////////////////////////////////////////////////////////////
+
+// Input and Output
+const fs = require("fs");
+const stdin = (
+  process.platform === "linux"
+    ? fs.readFileSync("/dev/stdin").toString()
+    : ``
+).split("\n");
+
+const input = (() => {
+  let line = 0;
+  return () => stdin[line++];
+})();
+
+////////////////////////////////////////////////////////////
+
+
+// Baekjoon JavaScript 
+// #2438
+
+// https://www.acmicpc.net/problem/2438
+
