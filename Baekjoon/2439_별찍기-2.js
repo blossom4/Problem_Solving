2143e8c878d24940014b3e5828702e30137f1a95
blossom4 <cho8213456@gmail.com>
2021-07-25 02:02:03 +0900

    Baejoon 2439 Solved

---
+++ Baekjoon/2439_별찍기-2.js
@@ -0,0 +1,42 @@
+////////////////////////////////////////////////////////////
+
+// Input and Output
+const fs = require("fs");
+const stdin = (
+  process.platform === "linux"
+    ? fs.readFileSync("/dev/stdin").toString()
+    : ``
+).split("\n");
+
+const input = (() => {
+  let line = 0;
+  return () => stdin[line++];
+})();
+
+////////////////////////////////////////////////////////////
+
+
+// Baekjoon JavaScript 
+// #2439  별 찍기 - 2
+
+// https://www.acmicpc.net/problem/2438
+
+
+const N = Number(input())
+let space = ' '
+let star = '*'
+let ans = ''
+
+// N줄에 걸쳐 별찍기 반복
+for (let i = 0; i < N; i++) {
+  // 공백부터 찍고 줄마다 반복횟수 -1
+  for (let j = 0; j < N - i - 1; j++) {
+    ans += space
+  }
+  // 공백 이후 별찍고 별 +1
+  ans += star
+  star += '*'
+  if (i != N) ans += '\n'
+}
+
+console.log(ans)
