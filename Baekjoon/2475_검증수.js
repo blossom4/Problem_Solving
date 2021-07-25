cccf00db24b790fd6da6599af5f2c02a396fa75a
blossom4 <cho8213456@gmail.com>
2021-07-25 12:55:58 +0900

    Baekjoon 2475 Solved

---
+++ Baekjoon/2475_검증수.js
@@ -0,0 +1,32 @@
+////////////////////////////////////////////////////////////
+
+// Input and Output
+const fs = require("fs");
+const stdin = (
+  process.platform === "linux"
+    ? fs.readFileSync("/dev/stdin").toString()
+    : `0 4 2 5 6`
+).split("\n");
+
+const input = (() => {
+  let line = 0;
+  return () => stdin[line++];
+})();
+
+////////////////////////////////////////////////////////////
+
+
+// Baekjoon JavaScript 
+// #2475  검증수
+
+// https://www.acmicpc.net/problem/2475
+
+
+const N = input().split(' ').map(Number)
+let total = 0
+
+for (let i = 0; i < 5; i++) {
+  total += N[i] ** 2
+}
+
+console.log(total % 10)
