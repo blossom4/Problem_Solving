3eb139ba213112d3f306f09fa536209cceeb261f
blossom4 <cho8213456@gmail.com>
2021-07-26 01:44:24 +0900

    LeetCode 33 Solved

---
+++ LeetCode/33_Search_in_Rotaed_Sorted_Array.js
@@ -0,0 +1,12 @@
+// LeetCode JavaScript
+// 33. Search in Rotated Sorted Array
+
+// https://leetcode.com/problems/search-in-rotated-sorted-array/
+
+
+// @param {number[]} nums
+// @param {number} target
+// @return {number}
+var search = function(nums, target) {
+  return nums.indexOf(target)
+};
