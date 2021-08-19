a2e44af0635324238206760bdd59fc61b1f79ef2
blossom4 <cho8213456@gmail.com>
2021-08-19 21:28:59 +0900

    modified

--- Java/src/q16394_홍익대학교/Main.java
+++ Java/src/q16394_홍익대학교/Main.java
@@ -1,18 +1,13 @@
-package q16170_오늘의날짜는;
+package q16394_홍익대학교;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.text.SimpleDateFormat;
-import java.util.Date;
 
 public class Main {
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		SimpleDateFormat format = new SimpleDateFormat("yyyy\nMM\ndd");
-		Date date = new Date();
-				
-		System.out.println(format.format(date));
 	}
+
 }
