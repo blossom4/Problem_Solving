f26d39d40d51a6aa991efefb39850d7a3ed8c3e9
blossom4 <cho8213456@gmail.com>
2021-08-20 17:03:29 +0900

    Yet

--- Java/src/q0_template/Main.java
+++ Java/src/q0_template/Main.java
@@ -6,9 +6,26 @@ import java.io.InputStreamReader;
 
 public class Main {
 	
+	// v: 현재속도, d: 도착지까지 남은 거리
+	public static int brakeDistance (int v) {
+		int sum = 0;
+		
+		for (int i = v; i > 0; i--) sum += i;
+		
+		return sum;
+	}
+	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int T = Integer.parseInt(br.readLine());
 		
+		for (int tc = 1; tc < T + 1; tc++) {
+			String[] input = br.readLine().split(" ");
+			int start = Integer.parseInt(input[0]);
+			int end = Integer.parseInt(input[1]);
+			
+			
+		}
 	}
 
 }
