bd4d4b1606cb7379e86123663b763af8f671fda8
blossom4 <cho8213456@gmail.com>
2021-09-04 16:03:03 +0900

    Baekjoon Java 14888 Solved

--- Java/src/q00000_test/Main.java
+++ Java/src/q00000_test/Main.java
@@ -6,6 +6,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 
+
 public class Main {
 	
 	public static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -13,7 +14,7 @@ public class Main {
 	
 	public static void main(String[] args) throws IOException {
 
-		
+	
 		bw.flush();
 		bw.close(); 
 		
